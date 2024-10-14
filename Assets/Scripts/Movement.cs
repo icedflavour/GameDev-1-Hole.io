526ed25f9caa7132cf76922b0b52819d29e1b102
f1avour <iced.flavour@gmail.com>
2024-10-14 19:26:32 +0300

    Added movement with rotation to the Hole

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,18 +6,28 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private int MovementSpeed;
 
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
-        if (Input.anyKey)
-        {
-            MoveTheHole();
-        }
-        
+        MoveTheHole();
     }
 
     private void MoveTheHole()
     {
+        float rotationInput = Input.GetAxis("Horizontal");
+        float movementInput = Input.GetAxis("Vertical");
 
+        transform.Rotate(0.0f, rotationInput, 0.0f, Space.Self);
+
+        rb.velocity = transform.forward * movementInput * MovementSpeed;
     }
 
+    //rb.velocity = new Vector3(Input.GetAxis("Horizontal") * MovementSpeed, 0f, Input.GetAxis("Vertical") * MovementSpeed);
 }
+
