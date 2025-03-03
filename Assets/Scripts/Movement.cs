b6b54f7ef0a25c9f5f36a21ecb3d9678f8d01c4a
f1avour <iced.flavour@gmail.com>
2025-03-03 09:43:46 +0200

    Renewed project

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,13 +6,6 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private int MovementSpeed;
 
-    private Rigidbody rb;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
     private void Update()
     {
         MoveTheHole();
@@ -23,12 +16,9 @@ public class Movement : MonoBehaviour
         float rotationInput = Input.GetAxis("Horizontal");
         float movementInput = Input.GetAxis("Vertical");
 
-        // Rotate the hole
         transform.Rotate(0.0f, rotationInput, 0.0f, Space.Self);
-
-        // Move the hole
+        
         Vector3 forwardMovement = transform.forward * movementInput * MovementSpeed * Time.deltaTime;
         transform.position += forwardMovement;
     }
 }
-
